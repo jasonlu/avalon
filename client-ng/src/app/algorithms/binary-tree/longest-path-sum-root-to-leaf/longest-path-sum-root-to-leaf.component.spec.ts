d7534bd0443e2ba85572f6f4a35da43e2742c560
Jason Lu <lujason@vmware.com>
2019-03-12 16:39:03 -0700

    Update client unit test

--- client-ng/src/app/algorithms/binary-tree/longest-path-sum-root-to-leaf/longest-path-sum-root-to-leaf.component.spec.ts
+++ client-ng/src/app/algorithms/binary-tree/longest-path-sum-root-to-leaf/longest-path-sum-root-to-leaf.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LongestPathSumRootToLeafComponent } from './longest-path-sum-root-to-leaf.component';
+import { ClarityModule } from "@clr/angular";
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from "@angular/forms";
+import { BinaryTreeService } from "../../services/binary-tree.service";
+import { HttpClientModule } from "@angular/common/http";
 
 describe('LongestPathSumRootToLeafComponent', () => {
   let component: LongestPathSumRootToLeafComponent;
@@ -8,6 +13,13 @@ describe('LongestPathSumRootToLeafComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        ClarityModule,
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [ BinaryTreeService ],
       declarations: [ LongestPathSumRootToLeafComponent ]
     })
     .compileComponents();
--- client-ng/src/app/algorithms/services/binary-tree.service.spec.ts
+++ client-ng/src/app/algorithms/services/binary-tree.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BinaryTreeService } from './binary-tree.service';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 describe('BinaryTreeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ],
+    providers: [ HttpClient ],
+  }));
 
   it('should be created', () => {
     const service: BinaryTreeService = TestBed.get(BinaryTreeService);
